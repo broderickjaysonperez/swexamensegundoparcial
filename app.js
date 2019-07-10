c2ec3dd23788b60b5d215532474008297958e450
broderick <brodjay@hotmail.com>
2019-07-09 19:49:56 -0600

    incorporacion post

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('api',apiRouter);
+app.use('/api',apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api/incidente.js
+++ routes/api/incidente.js
@@ -1,19 +1,82 @@
-
-
 var express = require('express');
 var router = express.Router();
+//3
+var ObjectID =require('mongodb').ObjectID;
+
+
 
 function initIncidente(db){
 
-    var incidenteColl = db.collection('incidente');
-router.get('/',(req, res ,next)=>{
-    res.status(200).json({"api":"1"});
-});
+    var incidenteColl =db.collection('incidente');
+    router.get('/',(req, res, next)=>{
+       
 
+
+
+        
+//3.............................===============
+
+    incidenteColl.find().toArray((err , incidente)=>{
+        if(err){
+            console.log(err);
+            return res.status(404).json({"error":"error al extraer incidente de la base"})
+        }
+            return res.status(200).json(incidente);
+    });
     
-return router;
+    });//get all
+    
 
+//==============3================================== 
+router.get('/:id',(req,res, next)=>{
 
+var id =new ObjectID(req.params.id);
+incidenteColl.findOne({"_id": id} , (err, doc)=>{
+if(err){
+    console.log(err);
+    return res.status(404).json({"error":"nose puede obtener incidente"});
 }
 
+return res.status(200).json(doc);
+});//findone
+
+});// 3//id
+
+
+router.post('/',(req, res, next ) =>{
+var newincidente = Object.assign({},
+    {
+    "descripcion":'',
+    "fechaHora":new Date().getTime(),
+    "estado":"",
+    "usuarioregistra":'',
+    "usuarioasignado" : "",
+    "fechaCreated": new Date().getTime(),
+    "fechaCerrado": new Date().getTime()
+    
+    },
+    req.body
+    );
+
+    //insertar 
+    incidenteColl.isertOne(newincidente,(err, rslt)=>{
+    if(err){
+    console.log(err);
+    return res.status(404).json({"error":"no se pudo agregar nueva incidente"});
+    }
+        if(rslt.ops.length===0){
+        console.log(rslt);
+        return res.status(404).json({"error":"no se pudo agregar nueva incidente"});
+    
+        }
+return res.status(200).json(rslt.ops[0]);
+
+    });
+
+});//post
+
+
+return router;
+}
 module.exports = initIncidente;
+
